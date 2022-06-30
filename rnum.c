dec15e3b7e571a4e60dab02cf419b7997a443012
Egg Milk Soup <egg@milksoup.net>
2022-06-30 13:36:00 -0500

    update code and handle errors

--- rnum.c
+++ rnum.c
@@ -3,21 +3,33 @@
 #include <openssl/rand.h>
 
 int main(int argc, char *argv[]) {
-	unsigned int bytes = sizeof(unsigned int);
+	if(argc != 2)
+	{
+		fprintf(stderr, "usage: %s max\n", argv[0]);
+		return 1;
+	}
+	int bytes = sizeof(long long);
 	unsigned char buf[bytes];
-	unsigned int max = atoi(argv[1]);
-	unsigned int randmax = UINT_MAX - (UINT_MAX - max + 1) % max;
-	unsigned int n = 0;
-	while(n == 0 || n > randmax) {
+	long long max = strtoll(argv[1], NULL, 10);
+	if(max < 1)
+	{
+		fprintf(stderr, "max must be a positive integer\n");
+		return 1;
+	}
+	long long randmax = LLONG_MAX - (LLONG_MAX - max + 1) % max;
+	long long n = 0;
+	while(n == 0 || n > randmax)
+	{
 		RAND_bytes(buf, bytes);
 		n = buf[0];
-		for(unsigned int byte = 1; byte < bytes; byte = byte + 1) {
-			unsigned int factor=256;
-			for(unsigned int pow = 2; pow <= byte; pow = pow + 1) {
+		for(int byte = 1; byte < bytes; byte = byte + 1)
+		{
+			int factor=256;
+			for(int pow = 2; pow <= byte; pow = pow + 1)
 				factor = factor * factor;
-			}
+
 			n = n + factor * buf[byte];
 		}
 	}
-	printf("%u\n", n % max + 1);
+	printf("%lld\n", n % max + 1);
 }
